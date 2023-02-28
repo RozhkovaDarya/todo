9d2398db7e366d51b706419af764a02233bab98c
Daria <mironochka1@gmail.com>
2023-02-28 14:32:53 +0300

    lesson_8 Created three tests

---
+++ todo/mainapp/tests.py
@@ -0,0 +1,37 @@
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import UserViewSet
+from .models import User, Notes
+
+
+class TestUserViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/users/', {'name': 'Андрей',
+                               'birthday_year': 1999}, format='json')
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/users/', {'name': 'Андрей',
+                               'birthday_year': 1999}, format='json')
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        force_authenticate(request, admin)
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    
