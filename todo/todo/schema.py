c4d82d7ff61bb4afd8528807831ba6b0bfa7dedd
Daria <mironochka1@gmail.com>
2023-03-18 11:07:15 +0300

    lesson_10 added data schema

--- todo/todo/schema.py
+++ todo/todo/schema.py
@@ -1,5 +1,25 @@
 import graphene
+from graphene_django import DjangoObjectType
+from mainapp.models import User, Notes
+
+
+class NotesType(DjangoObjectType):
+    class Meta:
+        model = Notes
+        fields = '__all__'
+    
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
-    schema = graphene.Schema(query=Query)
+    all_books = graphene.List(NotesType)
+    
+    def resolve_all_books(root, info):
+        return Notes.objects.all()
+        
+
+schema = graphene.Schema(query=Query)
